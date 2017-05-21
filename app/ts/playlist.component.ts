c2de804520dd8fa21aae328f271ad8666110d8c3
Muhammad Junaid <m.junaidcs@gmail.com>
2017-05-21 11:53:00 +0500

    Load data directly from same componenet instaed of as Input

--- app/ts/playlist.component.ts
+++ app/ts/playlist.component.ts
@@ -13,6 +13,23 @@ export class PlaylistComponent {
 
 	showArr: boolean = true;
 
+	// videos_ = [{
+	// 	id: 2,
+	// 	title: 'its title',
+	// 	desc: 'its desc'
+	// }];
+
+	videos_: Video[];
+
+	constructor() {
+		this.videos_ = [
+            new Video(1, 'Vid 1 title', 'vid1Code', 'vid1Desc here we goes'),
+            new Video(2, 'Vid 2 title', 'vid2Code', 'vid2Desc here we goes'),
+            new Video(3, 'Vid 3 title', 'vid3Code', 'vid3Desc here we goes'),
+            new Video(4, 'Vid 4 title', 'vid4Code', 'vid4Desc here we goes'),
+        ]
+	}
+
     clickCalled(vid:Video) {
         console.log(vid);
     }
